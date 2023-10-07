ab66bad20007df6f497e333ee0973a18121d1362
huonggiang0501 <141563689+huonggiang0501@users.noreply.github.com>
2023-10-07 09:58:31 +0700

    07.10

---
+++ HCG/HCG/src/main/java/com/hcg/HCG/HcgApplication.java
@@ -0,0 +1,13 @@
+package com.hcg.HCG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcgApplication.class, args);
+	}
+
+}
---
+++ HCG/HCG/src/test/java/com/hcg/HCG/HcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcg.HCG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
